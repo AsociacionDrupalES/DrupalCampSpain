4367a992b5a6cde83496bdbdb03012fafb51adfc
ckrina <cristina@ymbra.com>
2017-04-08 22:47:14 +0200

    [#146] Animate hamburguer menu icon

--- web/themes/dcamp_2017_theme/scripts/main.js
+++ web/themes/dcamp_2017_theme/scripts/main.js
@@ -2,11 +2,8 @@
 
   Drupal.behaviors.dcamp2017ThemeBehaviors = {
     attach: function (context, settings) {
-      $('.main-menu-trigger').click(function () {
-        $('body').addClass('main-menu-enabled');
-      });
-      $('.close-trigger').click(function () {
-        $('body').removeClass('main-menu-enabled');
+      $('.main-menu__trigger').click(function () {
+        $('body').toggleClass('main-menu-enabled');
       });
 
       $('#block-userlogin').prepend('<div class="user-login-trigger">'+Drupal.t('Login')+'</div>');
