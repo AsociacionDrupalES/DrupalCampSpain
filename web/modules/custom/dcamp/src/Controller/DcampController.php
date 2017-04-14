4c5da15f9b83063bcdca916683d1ec38af6abdf1
nuez <teun@nuezweb.com>
2017-04-14 19:00:22 +0200

    This closes #231, closes #230, closes #228, closes #226, closes #218, closes #221, closes #144

--- web/modules/custom/dcamp/src/Controller/DcampController.php
+++ web/modules/custom/dcamp/src/Controller/DcampController.php
@@ -28,7 +28,7 @@ class DcampController extends ControllerBase {
    * @param \Drupal\dcamp\Entity\Dcamp $dcamp
    * @return array
    */
-  public function landing(Dcamp $dcamp){
+  public function landing(Dcamp $dcamp) {
     // Return nothing. We only use blocks on the landing pages.
     return [
       '#markup' => '',
@@ -40,25 +40,33 @@ class DcampController extends ControllerBase {
    *
    * @todo Convert Service Container to Dependency Injection.
    */
-  public function frontpage(){
+  public function frontpage() {
     /** @var BlockManager $blockManager */
-    $blockManager =     \Drupal::getContainer()->get('plugin.manager.block');
+    $blockManager = \Drupal::getContainer()->get('plugin.manager.block');
 
-   return [
-     '#theme' => 'frontpage',
-     '#business_day' => $blockManager->createInstance('block_content:dcamp_2017_business_day')->build(),
-     '#venue' => $blockManager->createInstance('block_content:dcamp_2017_venue')->build(),
-     '#sponsors' =>  views_embed_view('sponsors', 'all_sponsors'),
-     '#become_a_sponsor' => $blockManager->createInstance('block_content:dcamp_2017_become_a_sponsor')->build(),
-     '#speakers' =>  views_embed_view('featured_speakers', 'block'),
-     '#community' => $blockManager->createInstance('block_content:dcamp_2017_community')->build(),
-     '#attached' => [
-       'library' => [
-         'dcamp_2017_theme/frontpage',
-         'dcamp_2017_landing_theme/plax'
-       ]
-     ]
-   ];
+    return [
+      '#theme' => 'frontpage',
+      '#business_day' => $blockManager->createInstance('block_content:dcamp_2017_business_day')->build(),
+      '#venue' => $blockManager->createInstance('block_content:dcamp_2017_venue')->build(),
+      '#sponsors' => views_embed_view('sponsors', 'all_sponsors'),
+      '#become_a_sponsor' => $blockManager->createInstance('block_content:dcamp_2017_become_a_sponsor')->build(),
+      '#speakers' => views_embed_view('featured_speakers', 'block'),
+      '#community' => $blockManager->createInstance('block_content:dcamp_2017_community')->build(),
+      '#attached' => [
+        'library' => [
+          'dcamp_2017_theme/frontpage',
+          'dcamp_2017_landing_theme/plax'
+        ]
+      ]
+    ];
+  }
+
+  /**
+   * Title callback for frontpage
+   */
+
+  public function frontpageTitle(){
+    return $this->t('Two days of Drupal | Madrid May 5-6');
   }
 
 }
