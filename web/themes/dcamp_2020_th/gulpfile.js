b66a02806d94f97f4f3e4fd443bcaf43aa9ffdb3
Marcelo Tosco @capynet <marcelo@front.id>
2019-12-09 21:01:35 +0100

    Added dcamp 2020 theme.

---
+++ web/themes/dcamp_2020_th/gulpfile.js
@@ -0,0 +1,71 @@
+// jshint ignore: start
+'use strict';
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+
+const settings = {
+  // Sources
+  sassSrcFiles: ['src/sass/**/*.scss'],
+  jsSrcFiles: ['src/js/**/*.js', '!src/js/vendor/**'],
+  copySrcFiles: ['src/js/*vendor/**/*', 'src/*img/**/*'],
+
+  // Destinations.
+  destDir: 'dist',
+};
+
+/**
+ * Compiles SASS files.
+ */
+const jsTask = () => {
+  return gulp.src(settings.jsSrcFiles)
+    .pipe(concat('theme.js'))
+    // .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(settings.destDir));
+};
+
+/**
+ * Compiles SASS files.
+ */
+const sassTask = () => {
+  return gulp.src(settings.sassSrcFiles)
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(settings.destDir));
+};
+
+// Copies static files to dist.
+const copyTask = () => {
+  return gulp
+    .src(settings.copySrcFiles)
+    .pipe(gulp.dest(settings.destDir));
+};
+
+/**
+ * Build task.
+ */
+const buildTask = gulp.parallel(
+  sassTask,
+  jsTask,
+  copyTask
+);
+
+/**
+ * Watcher.
+ */
+function watcherTask() {
+  buildTask();
+  gulp.watch(settings.sassSrcFiles, buildTask);
+  gulp.watch(settings.jsSrcFiles, buildTask);
+}
+
+
+exports.build = buildTask;
+exports.watch = watcherTask;
